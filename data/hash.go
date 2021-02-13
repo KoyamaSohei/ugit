8df4b13efd3fa04bd60c02098edd5cf71ef4b456
KoyamaSohei <koyamaso0309@gmail.com>
2021-02-13 09:24:04 +0900

    add hash

---
+++ data/hash.go
@@ -0,0 +1,21 @@
+package data
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+)
+
+// HashObject gen hash from data and save data.
+func HashObject(data []byte) []byte {
+	h := sha1.New()
+	if _, err := h.Write(data); err != nil {
+		panic(err)
+	}
+	bs := h.Sum(nil)
+	p := fmt.Sprintf("%s/objects/%x", GITDIR, bs)
+	if err := ioutil.WriteFile(p, data, 0755); err != nil {
+		panic(err)
+	}
+	return bs
+}
--- data/init.go
+++ data/init.go
@@ -1,11 +1,19 @@
 package data
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // GITDIR is git directory
 const GITDIR = ".ugit"
 
 // Init initialize .ugit
 func Init() {
-	os.Mkdir(GITDIR, os.FileMode(os.O_RDWR))
+	if err := os.MkdirAll(GITDIR, 0755); err != nil {
+		panic(err)
+	}
+	if err := os.MkdirAll(fmt.Sprintf("%s/objects", GITDIR), 0755); err != nil {
+		panic(err)
+	}
 }
